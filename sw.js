0b3a79520eeaf3e08df6ca8d183da12f231cf8be
Aidan <survivalking4@gmail.com>
2023-02-04 22:20:03 -0700

    grz

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-console.log("hi")
+self.addEventListener('fetch', event => {
+    const url = new URL(event.request.url);
+    event.respondWith((async () => {
+        const formData = await event.request.formData();
+        const link = formData.get('link') || '';
+        const responseUrl = await saveBookmark(link);
+        return Response.redirect("https://aidanjacobson.github.io/sharetest/recieve.html#file_recieved", 303);
+    })());
+  });
+  
