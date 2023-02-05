edfff46707e58c75afead97a6d50d0209972ceb5
Aidan <survivalking4@gmail.com>
2023-02-04 21:51:21 -0700

    ah

--- recieve_script.js
+++ recieve_script.js
@@ -1,3 +1,9 @@
 window.onload = window.onhashchange = function() {
     document.body.innerText = location.hash;
 }
+
+var sw;
+async function main() {
+    sw = await navigator.serviceWorker.register("sw.js");
+}
+main();
