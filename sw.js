71ec21347de075308bbfb71d3c61e6b6e5a49aa6
Aidan <survivalking4@gmail.com>
2023-02-04 22:35:24 -0700

    drgn

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
 self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
-    event.respondWith((async () => {
-        const formData = await event.request.formData();
-        const file = formData.get('files');
-        return Response.redirect("https://aidanjacobson.github.io/sharetest/recieve.html#file_recieved", 303);
-    })());
+    if (event.request.method === 'POST') {
+        event.respondWith((async () => {
+            const formData = await event.request.formData();
+            const file = formData.get('files');
+            return Response.redirect("https://aidanjacobson.github.io/sharetest/recieve.html#file_recieved", 303);
+        })());
+    }
   });
   
